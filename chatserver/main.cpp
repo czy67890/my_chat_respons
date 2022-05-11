ff00dca15d2f12aaa37a2586b2dd2e1c360802ac
czy67890 <2089808398@qq.com>
2022-05-11 19:06:14 +0800

    new version

---
+++ chatserver/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+#include<stdlib.h>
+
+#include "../muduo/base/Platform.h"
+#include "../muduo/base/Singleton.h"
+#include "../muduo/base/ConfigFileReader.h"
+#include "../muduo/base/AsyncLogging.h"
+#include "../muduo/net/EventLoop.h"
+#include "../muduo/net/EventLoopThreadPool.h"
+#include"../muduo/base/ConfigFileReader.h"
+
---
+++ chatserver/my_head.cc
@@ -0,0 +1,5 @@
+#include"chatserver/my_head.h"
+int my_add(int a,int b){
+    return a+b;
+}
+
